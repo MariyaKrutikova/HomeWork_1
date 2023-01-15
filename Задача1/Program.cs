14472240e92eff9c465477541918fc80b095d880
Mariya <appleslover@yandex.ru>
2023-01-15 23:31:04 +0300

    Задача 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+string numberStr1 = Console.ReadLine();
+int number1 = Convert.ToInt32(numberStr1);
+
+Console.WriteLine("Введите второе число: ");
+string numberStr2 = Console.ReadLine();
+int number2 = Convert.ToInt32(numberStr2);
+
+if (number1 > number2){
+    Console.WriteLine(number1 + " больше, " + number2 + " меньше");
+}
+else Console.WriteLine(number2 + " больше, " + number1 + " меньше");
